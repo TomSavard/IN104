01c5d3ebdca9f2b179b6158806d4beb09d64f213
savard <tom.savard@orange.fr>
2023-04-04 17:52:21 +0200

    ajout de l'écriture de .txt dans générateur

--- generateur.c
+++ generateur.c
@@ -7,28 +7,40 @@
 int plaisir_max=10;
 int croisement_max=6;
 int main(){
+    // on stock les données dans un txt
+    FILE *f;
+    f = fopen("./data/test.txt", "w");
+    if(f==NULL){
+    printf("Erreur lors de l'ouverture d'un fichier\n");
+    exit(1);}
+
     //on génère le nombre de croisement
     time_t t;
     t = time(NULL);
     srand(t);
-    int n=rand()%croisement_max;
-    printf("n=%d\n",n);
+    int n=2+rand()%croisement_max;//ici 2 croisements minimum
 
     int m=0;
-
+    int nbr_piste[n];
     // pour chaque croisement on génère un nombre de piste (avec nbr piste <= nbr croisement-1)
     for (int xi=0; xi<n; xi++){
-        int* existe=calloc(n,sizeof(int)); // vecteur pour l'existence de piste sur ce croisement
         int p=rand()%(n-1);//nbr de piste adjacente au croisement
+        nbr_piste[xi]=p;
         m=m+p;
-        for (int i=0; i<p; i++){ //pour chaque piste partant de xi, on prend un croisement d'arriver yi et un plaisir fi
+    }
+    fprintf(f,"%d %d\n",n,m);
+
+    //puis on défini l'aarivé et le plaisir de chaque piste
+    for(int xi=0; xi<n; xi++){
+        int* existe=calloc(n,sizeof(int)); // vecteur pour l'existence de piste sur ce croisement
+        for (int i=0; i<nbr_piste[xi]; i++){ //pour chaque piste partant de xi, on prend un croisement d'arriver yi et un plaisir fi
             existe[xi]=1; //une piste ne revient pas au même croisement
             int yi=rand()%(n);
             while (existe[yi]==1){
                 yi=rand()%(n-1);}
             existe[yi]=1;
             int fi=rand()%plaisir_max;
-            printf("xi=%d  yi=%d  fi=%d\n",xi,yi,fi);
-    }}
-    printf("m=%d\n",m);
+            fprintf(f,"%d %d %d\n",xi,yi,fi);
+        }
+    }
 }
