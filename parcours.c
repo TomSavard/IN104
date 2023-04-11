5ad85a77be8725a2cfd59e2119ca375d9303b346
savard <tom.savard@orange.fr>
2023-04-11 15:00:37 +0200

    prog de parcours

---
+++ parcours.c
@@ -0,0 +1,9 @@
+/*Tom SAVARD 11 avril*/
+// Ce programme a pour objectif de parcourir le graph et de calculer le plaisir optimal 
+
+// On a un plaisir pouvant être positif ou négatif. 
+// Il faut être capable de détecter les cycles positifs ==> boucle infini
+
+#include <stdio.h>
+#include <stdlib.h>
+
